d061d29eac8d7e8c9924c37d326587084e0d7940
liangke21 <1396797522@qq.com>
2021-11-20 03:54:55 +0800

    更改项目完成

--- 05声明和定义/05声明和定义/fileD.cpp
+++ 05声明和定义/05声明和定义/fileD.cpp
@@ -3,4 +3,4 @@
 //int ii = 43; // LNK2005! 'i' 已经有了定义。
 //extern int ii = 43; // 相同的错误（定义时忽略 extern）
 
-//extern constexpr int ii = 10; //error LNK2005: "int const x" already define   13
+//extern constexpr int ii = 10; //error LNK2005: "int const x" already define   134  66666
