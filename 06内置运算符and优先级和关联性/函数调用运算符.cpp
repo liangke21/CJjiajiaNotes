b84a8757afda6966f28ede84586de192fdf17122
liangke21 <1396797522@qq.com>
2021-11-21 22:12:34 +0800

    函数调用运算符

---
+++ 06内置运算符and优先级和关联性/函数调用运算符.cpp
@@ -0,0 +1,39 @@
+// expre_Function_Call_Operator.cpp
+// compile with: /EHsc
+
+#include <iostream>
+#include <string>
+
+// C++ Standard Library name space
+using namespace std;
+
+int main151()
+{
+    enum
+    {
+        sizeOfBuffer = 20
+    };
+
+    char s1[sizeOfBuffer] = "Welcome to ";
+    char s2[] = "C++";
+
+    strcat_s(s1, sizeOfBuffer, s2);
+   /*
+   第一个参数
+Null 终止的目标字符串缓冲区。
+
+第二个参数
+目标字符串缓冲区的大小。
+
+第三个参数
+以 null 结尾的源字符串缓冲区。
+
+locale
+要使用的区域设置。
+   */
+
+   
+    cout << s1 << endl;  //C++
+
+    return 0;
+}
