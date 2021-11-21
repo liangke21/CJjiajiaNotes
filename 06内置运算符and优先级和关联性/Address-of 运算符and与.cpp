fee605abacdd44103a12222d0b94a1e76f64e8c0
liangke21 <1396797522@qq.com>
2021-11-21 19:16:48 +0800

    操作员地址

---
+++ 06内置运算符and优先级和关联性/Address-of 运算符and与.cpp
@@ -0,0 +1,58 @@
+
+
+//示例：静态成员的地址
+
+// expre_Address_Of_Operator.cpp
+// C2440 expected
+class PTM {
+public:
+    int iValue;
+    static float fValue;
+};
+
+int main4() {
+    int   PTM::* piValue = &PTM::iValue;  // OK: non-static
+  //  float PTM::* pfValue = &PTM::fValue;  // C2440 error: static
+  //  float* spfValue = &PTM::fValue;  // OK
+
+    return 0;
+}
+
+//示例：引用类型的地址
+
+// expre_Address_Of_Operator2.cpp
+// compile with: /EHsc
+#include <iostream>
+using namespace std;
+int main5() {
+    double d=3.14;        // Define an object of type double.
+    double& rd = d;  // Define a reference to the object.
+
+    // 获取并比较他们的地址
+    if (&d == &rd)
+        cout << "&d equals &rd" << endl;
+
+    cout << "d 地址 : " << &d << endl;
+
+    cout << "rd 地址 : " << &rd << endl;
+    return 0;
+}
+
+//示例：作为参数的函数地址
+
+// expre_Address_Of_Operator3.cpp
+// compile with: /EHsc
+// Demonstrate address-of operator &
+
+#include <iostream>
+using namespace std;
+
+// Function argument is pointer to type int
+int square(int* n) {
+    return (*n) * (*n);
+}
+
+int main() {
+    int mynum = 5;
+    cout << square(&mynum) << endl;   // pass address of int
+}
--- 06内置运算符and优先级和关联性/加法运算符：+ 和 -.cpp
+++ 06内置运算符and优先级和关联性/加法运算符：+ 和 -.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 //加法运算符：+ 和 -
-int main() {
+int main3() {
     int i = 5, j = 10;
     int n[SIZE] = { 0, 1, 2, 3, 4 };
     cout << "5 + 10 = " << i + j << endl
@@ -20,5 +20,27 @@ int main() {
     cout << "n[3] = " << *&(n[2]) << endl;
     cout << "n[3] = " << &n[2] << endl;
 
+    return 0;
+}
 
+
+//指针加减法
+int main03()
+{
+
+    short IntArray[10]; // Objects of type short occupy 2 bytes
+    short* pIntArray = IntArray;
+
+    for (int i = 0; i < 10; ++i)
+    {
+        *pIntArray = i;
+        cout << *pIntArray << "\n";
+        pIntArray = pIntArray + 1;  //虽然将整数值 1 添加到 pIntArray，但这并不表示“将 1 添加到该地址”，而是指“调整指针使其指向数组中的下一个对象”，而该对象恰好是在 2 字节（或者 sizeof( int )）之外。
+    }
+
+    for (short a : IntArray) {
+
+        cout << "a : " << a<< endl;
+    }
+    return 0;
 }
