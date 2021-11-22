b1407f95311edcc988ef0e40b9b13f30751ca747
liangke21 <1396797522@qq.com>
2021-11-22 16:00:57 +0800

    主表达式

--- 06内置运算符and优先级和关联性/typeid 运算符.cpp
+++ 06内置运算符and优先级和关联性/typeid 运算符.cpp
@@ -11,7 +11,7 @@ public:
 class Derived : public Base {};
 
 using namespace std;
-int main() {
+int main320() {
 	Derived* pd = new Derived;
 	Base* pb = pd;
 	cout << typeid(pb).name() << endl;   //prints "class Base *"
@@ -19,5 +19,29 @@ int main() {
 
 	cout << typeid(pd).name() << endl;   //prints "class Derived *"
 	cout << typeid(*pd).name() << endl;   //prints "class Derived"
+
+
 	delete pd;
+
+	return 0;
+}
+
+
+
+int main322()
+{
+	typeid(int) == typeid(int&); // 评估为真
+
+	return 0;
+}
+
+
+//typeid 还可以在模板中用于确定模板参数的类型：
+// expre_typeid_Operator_3.cpp
+// compile with: /c
+#include <typeinfo>
+template < typename T >
+T max(T arg1, T arg2) {
+	cout << typeid(T).name() << "s compared." << endl;
+	return (arg1 > arg2 ? arg1 : arg2);
 }
