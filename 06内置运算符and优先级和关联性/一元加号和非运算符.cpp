5f79b71fea2406e5c7b0f8f7eb2cf183e7de4e62
liangke21 <1396797522@qq.com>
2021-11-22 17:11:45 +0800

    一元加号运算符合非运算符

---
+++ 06内置运算符and优先级和关联性/一元加号和非运算符.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+
+int main331()
+{
+	using namespace std;
+	//一元加号运算符
+	int aa = 4;
+	int a = 36+aa;
+
+	//一元非运算符
+	int b = 4;
+	int d=- b;
+	cout << "+ : " << a<<endl
+		<< " - : " << d << endl;
+
+	return 0;
+}
+
+
