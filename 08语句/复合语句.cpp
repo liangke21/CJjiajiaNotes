746d141865c18dae5702b80b00e3247382b9e177
liangke21 <1396797522@qq.com>
2021-11-23 20:10:46 +0800

    复合语句

---
+++ 08语句/复合语句.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+
+    if (101 > 100)
+    {
+        cout << "我是如果\n";
+        
+    }
+    else
+    {
+        cout << "我是否则\n";
+    }
+
+}
