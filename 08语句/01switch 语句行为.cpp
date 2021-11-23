e1ffdafb565a70099373688579cacef01864ea31
liangke21 <1396797522@qq.com>
2021-11-23 23:02:24 +0800

    switch语句

---
+++ 08语句/01switch 语句行为.cpp
@@ -0,0 +1,34 @@
+
+
+// switch_statement1.cpp
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+    const char* buffer = "任意字符流";
+    int uppercase_A, lowercase_a, other;
+    char c;
+    uppercase_A = lowercase_a = other = 0;
+
+    cout << "buffer222 : " << buffer<< endl;
+
+    while (c = *buffer++)   // 遍历缓冲区直到 NULL
+    {
+        cout << "buffer : " << buffer<<c<<endl;
+
+        switch (c)
+        {
+        case 'A':
+            uppercase_A++;
+            break;
+        case 'a':
+            lowercase_a++;
+            break;
+        default:
+            other++;
+        }
+    }
+    printf_s("\nUppercase A: %d\nLowercase a: %d\nTotal: %d\n",
+        uppercase_A, lowercase_a, (uppercase_A + lowercase_a + other));
+}
