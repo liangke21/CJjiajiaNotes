9141acff76e1b3343283cb66d1c45a612567bfee
liangke21 <1396797522@qq.com>
2021-11-28 00:36:10 +0800

    用于编译时封装的Pimpll

---
+++ 11类和结构/my_class.cpp
@@ -0,0 +1,9 @@
+#include "my_class.h"
+//class my_class::impl {  // 在这里私下定义
+//  // ... 所有私有数据和功能：所有这些
+//  //     现在可以在不重新编译调用者的情况下进行更改...
+//};
+//my_class::my_class() : pimpl(new impl)
+//{
+//	// ... 设置 impl 值 ...
+//}
